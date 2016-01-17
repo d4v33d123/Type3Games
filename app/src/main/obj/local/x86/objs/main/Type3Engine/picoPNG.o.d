8db962ddccbf35017aca4d46536eb7f05cf75952
Davide <d.passaniti@gmail.com>
2016-01-17 15:26:48 +0000

    removed files comitted before .gitignore was added

--- app/src/main/obj/local/x86/objs/main/Type3Engine/picoPNG.o.d
+++
@@ -1,8 +0,0 @@
-C:/Users/Da/Desktop/T3Android/app/src/main/obj/local/x86/objs/main/Type3Engine/picoPNG.o: \
- C:/Users/Da/Desktop/T3Android/app/src/main/jni/src/Type3Engine/picoPNG.cpp \
- C:/Users/Da/Desktop/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
- C:/Users/Da/Desktop/T3Android/app/src/main/jni/src/../NDK/stddef.h
-
-C:/Users/Da/Desktop/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
-
-C:/Users/Da/Desktop/T3Android/app/src/main/jni/src/../NDK/stddef.h:
