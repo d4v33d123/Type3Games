0260391d019d8e312e50006c0eb01fb9d5123e40
Davide <d.passaniti@gmail.com>
2016-01-17 16:51:14 +0000

    fixed build path in BUILD_NATIVE_CODE.bat to match repo structure

--- BUILD_NATIVE_CODE.bat
+++ BUILD_NATIVE_CODE.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Make sure your ndk folder is one folder up from this batch file's location
+::Make sure your ndk folder is one folder up from this batch file's location
 echo Compiling, please wait...
 echo ---
 
@@ -7,8 +7,7 @@ echo ---
 cd ../android-ndk-r10e
 
 ::ndk-build in the specified path
-call ndk-build -C ..\Type3Games-android\app\src\main\jni
+call ndk-build -C ..\Type3Games\app\src\main\jni
 
 echo ---
-echo You can now run from within Android Studio
 pause
