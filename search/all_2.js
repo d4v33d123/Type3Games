7d9d84d519ef3425669b27cc88ad84d2c9c5cb33
Thomas Hope <hope.thomasj@gmail.com>
2016-03-06 02:21:52 +0000

    configfile.h now in docs

--- search/all_2.js
+++ search/all_2.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['camera',['Camera',['../class_t3_e_1_1_camera.html',1,'T3E']]],
   ['cell',['Cell',['../class_t3_e_1_1_cell.html',1,'T3E']]],
-  ['colour',['Colour',['../struct_t3_e_1_1_colour.html',1,'T3E']]]
+  ['colour',['Colour',['../struct_t3_e_1_1_colour.html',1,'T3E']]],
+  ['configfile',['ConfigFile',['../class_t3_e_1_1_config_file.html',1,'T3E']]]
 ];
