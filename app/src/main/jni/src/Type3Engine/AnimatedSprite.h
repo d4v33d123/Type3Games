6533c194405ef450b34b051a02a339ca852cee7d
David Robertson <David Robertson>
2016-04-05 19:35:42 +0100

    merge

--- app/src/main/jni/src/Type3Engine/AnimatedSprite.h
+++ app/src/main/jni/src/Type3Engine/AnimatedSprite.h
@@ -20,26 +20,54 @@ namespace T3E
 	public:
 		AnimatedSprite();
 		~AnimatedSprite();
-
+		
+		/**
+		* Initialise the Animated sprite
+		*
+		* @param [in] x position in the world
+		* @param [in] y position in the world
+		* @param [in] width of the sprite
+		* @param [in] height of the sprite
+		* @param [in] texture file path
+		* @param [in] UV position x on the spritesheet
+		* @param [in] UV position y on the spritesheet
+ 		* @param [in] width of a sprite on the spritesheet
+		* @param [in] height of a sprite on the spritesheet
+		* @param [in] the number of sprites in the animation
+		* @param [in] the number of frames in each line of the sprite sheet
+		*/
 		//UV COORDS ARE SWAPPED!!! invert width with height and x with y for tiling, position in spritesheet etc
 		void init(float x, float y, float width, float height, std::string texturePath,
 			float tileX, float tileY, float tileWidth, float tileHeight, int numSprites, int framesPerLine);
 
+		/**
+		* Draw the sprite
+		*/
 		void draw();
 		
+		/**
+		* Update the animated sprite
+		* 
+		* @param [in] delta time
+		* @return [out] false if the sprite fails to update, true if it succeeds
+		*/
 		//return true when reached end frame of animation
 		bool Update(float deltaTime);
 
+		/**
+		* Sets the speed of the animation
+		*
+		* @param [in] a multiplier of deltatime to control the speed of the animation
+		*/
 		void setSpeed(float s){animSpeed_ = s;};
 		
 	private:
-		int animPosition_;
-		float counter;
-		float animSpeed_;
-		int numSprites_;
-		float animCount_;
-		float animEnd_;
-		int framesPerLine_;
+		int animPosition_; // The position of the animation out of the full animation
+		float animSpeed_; // multiplier of delta time to control the speed of the animation
+		int numSprites_; // the number of sprites in the animation
+		float animCount_; // the time since the last frame change
+		float animEnd_; // the time between each fram change
+		int framesPerLine_; // the number of frames in each line of the spritesheet
 	};
 
 }
--- app/src/main/jni/src/Type3Engine/Button.h
+++ app/src/main/jni/src/Type3Engine/Button.h
@@ -11,20 +11,47 @@ namespace T3E
 	class Button
 	{
 	public:
+		
+		/**
+		* Initialise the button
+		*
+		* @param [in] the x position of the button on the screen
+		* @param [in] the y position of the button on the screen
+		* @param [in] the width of the button sprite
+		* @param [in] the height of the button sprite
+		* @param [in] texture file path
+ 		* @param [in] width of a sprite on the spritesheet
+		* @param [in] height of a sprite on the spritesheet
+		* @param [in] the UV x position of the unpressed sprite on the spritesheet
+		* @param [in] the UV y position of the unpressed sprite on the spritesheet
+		* @param [in] the UV x position of the pressed sprite on the spritesheet
+		* @param [in] the UV y position of the pressed sprite on the spritesheet
+		*/
 		void init(float posX, float posY, float width, float height,
 			std::string texturePath, float tileWidth, float tileHeight,
 			float unpressedX, float unpressedY,
 			float pressedX, float pressedY);
 			
+		/**
+		* checks to see if the button has been pressed by the player
+		*
+		* @param [in] touch position in screen coordinates
+		* @return [out] return true if the coordinates lie inside the sprite, false if they do not
+		*/
 		//[in] touch position in screen coords
 		//[ret] true if touch coords lie inside button sprite
 		// test is made on unpressedSprite_
 		bool touchCollides(glm::vec2 touchPos);
 		
+		/**
+		* draws the sprite
+		*/
 		//draw correct sprite depending if it's pressed or unpressed
 		void draw();
 		
+		
 		//Setters
+		
 		void press(){isPressed_ = true;};
 		void unpress(){isPressed_ = false;};
 		void toggle(){isPressed_ = !isPressed_;};
