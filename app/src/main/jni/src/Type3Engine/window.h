ea9d0825690e46d25e6bd1b37775dcb0caf2dd13
Davide <d.passaniti@gmail.com>
2016-04-22 03:34:17 +0100

    Updated Documentation

--- app/src/main/jni/src/Type3Engine/window.h
+++ app/src/main/jni/src/Type3Engine/window.h
@@ -24,14 +24,47 @@ namespace T3E
 		window();
 		~window();
 
+        /**
+		*
+		* Create a window
+        * Note: on android the dimensions will be overwritten to fit the full screen size
+		*
+		* @param [in] name of the window (e.g. the one displayed in the toolbar on pc)
+		* @param [in] width of the window in pixels
+        * @param [in] height of the window in pixels
+		* @param [in] SDL flags for window options (e.g BORDERLESES, FULLSCREEN, ...)
+        *
+		*/
 		int create( std::string windowName, int screenWidth, int screenHeight, unsigned int currentFlag );
+        
+        /**
+		*
+		* Free the context and  the SDL_window member
+        *
+		*/
 		void destroy();
 
+        /**
+		*
+		* Swap the current buffer with the other one to display its content on screen (double buffering)
+        *
+		*/
 		void swapBuffer();
 		
+        /**
+		*
+		* load the actual current window dimensions in the screenWidth_ and screenHeight_ members
+        * -use this at least once before using getters for the dimensions of the window
+        * -use this again if the window size changes (e.g. on a phone, landscape to normal layout switch)
+        *
+		*/
 		void updateSizeInfo(); // updatescreenWidth_ and screenHeight_ with the current window size
+        
+        //@{
+        /** Getters for window size*/
 		int getScreenWidth() { return screenWidth_; }
 		int getScreenHeight() { return screenHeight_; }
+        //@}
 		
 	private:
 		SDL_Window* sdlWindow_;
